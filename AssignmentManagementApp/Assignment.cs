0f8f1db2a86adb53b7d6695449768a6add3b3739
Lee Houk <leehouk@gmail.com>
2025-04-09 20:10:19 -0700

    Add project files.

---
+++ AssignmentManagementApp/Assignment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentManagementApp
+{
+    public class Assignment
+    {
+        public required string Title { get; set; }
+    }
+}
---
+++ AssignmentManagementApp/AssignmentTests.cs
@@ -0,0 +1,14 @@
+﻿namespace AssignmentManagementApp
+{
+    public class AssignmentTests
+    {
+        [Fact]
+        public void Assignment_Should_Have_A_Title()
+        {
+            var assignment = new Assignment() {
+                Title = "Default Title"
+            };
+            Assert.NotNull(assignment.Title);
+        }
+    }
+}
